aa5f9ccb1d0d037db456931c7486bd7863015c3a
Eder Sato <edersato@gmail.com>
2020-07-28 17:45:50 -0300

    Aula 2 atualizações

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+const PaginaErro = () => (<div>
+  <h1>Ah não, sua página foi raptada pelo Kahl Uork</h1>
+  <h2>Enquanto você espera, ajude Hipsta a encontrar seu livro de poções e quem sabe, sua página</h2>
+  
+  <iframe src="https://editor.p5js.org/Eder_Sato/embed/SYVpVFLIz"></iframe>
+</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route component={PaginaErro} />
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
